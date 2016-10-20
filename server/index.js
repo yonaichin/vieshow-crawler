e17ea58d72f87ca76d500f137fcfa785bdb80960
Olly.W <yonaichin@gmail.com>
2016-10-20 16:05:47 +0800

    use node_env to determine port

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var TheaterList = require('../src/data/theater_list.js');
 
+app.set('port', (process.env.PORT || 3000));
+
 app.get('/', function (req, res) {
   res.send('VIESHOW Cinemas Showtimes!');
 });
@@ -9,6 +11,8 @@ app.get('/theater', function (req, res) {
   res.json(TheaterList);
 });
 
-app.listen(3000, function () {
-  console.log('VIESHOW crawler now running on http://localhost:3000');
+app.listen(app.get('port'), function() {
+  console.log('VIESHOW crawler now running on http://localhost:' + app.get('port'));
 });
+
+
